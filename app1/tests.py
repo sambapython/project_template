c7d40c3982d3930ba973c9ba70e21e0a95d49eb4
sambapython@gamil.com <sambapython@gamil.com>
2023-12-21 14:32:27 +0530

    added ci file

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from app1.models import Cricketers
+
+
+# models test
+class CricketersTest(TestCase):
+
+    def create_Cricketers(self, title="only a test", body="yes, this is only a test"):
+        return Cricketers.objects.create(name=title, image_path=body, age=7)
+
+    def test_Cricketers_creation(self):
+        w = self.create_Cricketers()
+        self.assertTrue(isinstance(w, Cricketers))
+      
